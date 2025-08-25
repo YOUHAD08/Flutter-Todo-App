f4aa105407eeefbaadec9b023513a6fc4d435cd1
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-08-25 19:45:55 +0100

    feat(todo): add CompletedTodoPage with slidable delete, update HomePage to handle active and completed todos with navigation

---
+++ lib/pages/completed.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:to_do_app/providers/todo_provider.dart';
+
+class CompletedTodoPage extends ConsumerWidget {
+  const CompletedTodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    List todos = ref.watch(todoProvider);
+    List completedTodos = todos.where((todo) => todo.isCompleted).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Completed Todo'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 87, 131, 153),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: ListView.builder(
+          itemCount: completedTodos.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Slidable(
+                startActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (context) {
+                        ref
+                            .read(todoProvider.notifier)
+                            .removeTodo(completedTodos[index].id);
+                      },
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ],
+                ),
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 0,
+                    horizontal: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(
+                      255,
+                      191,
+                      192,
+                      191,
+                    ), // background moves too
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      todos[index].content,
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                      textAlign: TextAlign.center,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:to_do_app/pages/add.dart';
+import 'package:to_do_app/pages/completed.dart';
+import 'package:to_do_app/providers/todo_provider.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List todos = ref.watch(todoProvider);
+    List activeTodos = todos.where((todo) => !todo.isCompleted).toList();
+    List completedTodos = todos.where((todo) => todo.isCompleted).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo App'),
@@ -22,7 +24,101 @@ class _MyHomePageState extends State<MyHomePage> {
           fontWeight: FontWeight.bold,
         ),
       ),
-      body: Center(child: Column()),
+      body: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: ListView.builder(
+          itemCount: activeTodos.length + 1,
+          itemBuilder: (context, index) {
+            if (index == activeTodos.length) {
+              if (completedTodos.isEmpty) {
+                return Container();
+              } else {
+                return Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => CompletedTodoPage(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Completed Todos',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              }
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Slidable(
+                  startActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          ref
+                              .read(todoProvider.notifier)
+                              .removeTodo(activeTodos[index].id);
+                        },
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ],
+                  ),
+                  endActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          ref
+                              .read(todoProvider.notifier)
+                              .toggleTodoCompletion(activeTodos[index].id);
+                        },
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                        icon: Icons.check,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ],
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 0,
+                      horizontal: 4,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(
+                        255,
+                        191,
+                        192,
+                        191,
+                      ), // background moves too
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        todos[index].content,
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                        textAlign: TextAlign.center,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(
